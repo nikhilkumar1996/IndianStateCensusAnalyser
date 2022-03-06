f438295c431049f902808a6543c9608a61699573
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-03-06 23:03:22 +0530

    [Nikhil Kumar]:Load andAnalyse IndianStateCensus info from Csv file

---
+++ ISCAnalyser/CensusAnalyser.cs
@@ -0,0 +1,26 @@
+﻿using ISCAnalyser.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISCAnalyser.POCO
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+    }
+}
